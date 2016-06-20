a30b6a1bd433a5475376c0f278034c8399ac41f4
nachuan <nachuan@nachuandeMac-mini.local>
2016-06-20 13:58:36 +0800

    recommit

---
+++ SKTabBar/SKTabBar/SKTabBarItem.h
@@ -0,0 +1,39 @@
+//
+//  SKTabBarItem.h
+//  SKTabBar
+//
+//  Created by nachuan on 16/6/17.
+//  Copyright © 2016年 SoKon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKTabBarItem : UIButton
+
+
+
+
+/** 图片占整个item的比例 default 0.7 */
+@property (nonatomic, assign) CGFloat ratio;
+
+/** 用于保存系统tabBarItem数据 */
+@property (nonatomic, strong) UITabBarItem *item;
+
+/** 默认为nil */
+@property (nonatomic, copy) NSString *badgeValue;
+
+/** 按钮的点击事件 */
+@property (nonatomic, strong) UITapGestureRecognizer *tap;
+
+/** normal 文字颜色 */
+@property (readonly, nonatomic, strong) UIColor *titleColor;
+
+/** selected 文字颜色 */
+@property (readonly, nonatomic, strong) UIColor *titleSelectedColor;
+
+/** normal 图片 */
+@property (readonly, nonatomic, strong) UIImage *image;
+
+/** selected 图片 */
+@property (readonly, nonatomic, strong) UIImage *selectedImage;
+@end
